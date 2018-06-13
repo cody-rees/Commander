ee88f932e3cf096da1895a1ff8e094d4bb5102ee
codyreesws <cody@designteam.ws>
2018-06-14 10:54:12 +1200

    Development Build

---
+++ CommanderTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using Commander;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommanderTests {
+    class Program {
+        static void Main(string[] startArgs) {
+            var commander = new CommanderCLI();
+
+            while (commander.ReadLine(out string[] args, out string[] flags)) {
+                Console.WriteLine("Arguments: ");
+                Console.WriteLine(string.Join("\n", args.Select(arg => $@"- ""{arg}""")));
+
+
+                Console.WriteLine("Flags: ");
+                Console.WriteLine(string.Join("\n", flags.Select(flag => $@"- ""{flag}""")));
+            }
+
+            Console.WriteLine("GoodBye");
+        }
+    }
+}
